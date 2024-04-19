63d10a6f245401b0bacd39fa2c42dc61aac21322
exdal <63502313+exdal@users.noreply.github.com>
2024-04-19 21:00:16 +0300

    xmake

---
+++ Lorr/Engine/Graphics/VK/Instance.hh
@@ -0,0 +1,8 @@
+//
+// Created by PC on 3/13/2024.
+//
+
+#ifndef INSTANCE_HH
+#define INSTANCE_HH
+
+#endif //INSTANCE_HH
---
+++ Lorr/Engine/STL/FixedVector.hh
@@ -0,0 +1,8 @@
+//
+// Created by PC on 3/12/2024.
+//
+
+#ifndef FIXEDVECTOR_HH
+#define FIXEDVECTOR_HH
+
+#endif //FIXEDVECTOR_HH
